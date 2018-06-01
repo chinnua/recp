bbb2156c4110021c08473f03b3968a5fa30e55e1
Arul Sembiyann <chinnu.a@gmail.com>
2018-06-01 18:02:28 +0530

    Initial

---
+++ Rep/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+
+            var fileHtml = File.ReadAllText(Path.Combine(Application.StartupPath, "Template", "Template.html"));
+            var filePath = "D:\\Test.pdf";
+
+            fileHtml = fileHtml.Replace("{{amount}}", "1000.00");
+            fileHtml = fileHtml.Replace("{{amountinwords}}", Helpers.CurrencyToWords.ConvertToWords("1000.00"));
+            fileHtml = fileHtml.Replace("{{name}}", "ABCDEFGH");
+            fileHtml = fileHtml.Replace("{{point}}", "Chennai");
+
+            var fileBytes = PdfSharpConvert(fileHtml);
+            File.WriteAllBytes(filePath, fileBytes);
+            using (View viewForm = new View(filePath))
+            {
+                viewForm.ShowDialog();
+            }
+
+        }
+
+        public static Byte[] PdfSharpConvert(String html)
+        {
+            Byte[] res = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var pdf = TheArtOfDev.HtmlRenderer.PdfSharp.PdfGenerator.GeneratePdf(html, PdfSharp.PageSize.A4);
+                pdf.Save(ms);
+                res = ms.ToArray();
+            }
+            return res;
+        }
+    }
+}
---
+++ Rep/View.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rep
+{
+    public partial class View : Form
+    {
+        private string pdfFileToRender = string.Empty;
+        public View(string pdfPath)
+        {
+            InitializeComponent();
+            pdfFileToRender = pdfPath;
+        }
+
+        private void View_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigated += WebBrowser1_Navigated;
+            webBrowser1.Navigate(pdfFileToRender);
+
+        }
+
+        private void WebBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            printDialog1.ShowDialog();
+        }
+    }
+}
